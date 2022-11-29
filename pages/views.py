58d11739b40e3a321fcab5d0b5a864fb6cb2a913
Anya Smith <anakrokhina@hotmail.com>
2022-11-29 14:18:10 -0800

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
-#from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
